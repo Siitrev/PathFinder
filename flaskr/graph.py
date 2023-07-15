87fff6b81ff868cf2a55183178c733e7a0cc5608
Siitrev <paplata50@gmail.com>
2023-07-15 13:21:20 +0200

    First try with uploading files

--- flaskr/graph.py
+++ flaskr/graph.py
@@ -1,6 +1,7 @@
 import igraph
+import matplotlib.pyplot as plt
 from flask import (
-    Blueprint, flash, redirect, render_template, request, session, url_for, make_response
+    Blueprint, flash, redirect, render_template, request, url_for, make_response
 )
 from flaskr.util import create_graph
 
@@ -22,24 +23,38 @@ def set_weights():
         return resp
 
 @bp.route('/show', methods=('GET', 'POST'))
-def show_graph():
+def show():
     code = 400
     if request.method == "POST":
-        tmp_edges = request.cookies.get("edges", None)[:-1] + "]"
+        tmp_edges = request.cookies.get("edges", None)[:-1]
+        edges = None
+        if tmp_edges:
+            tmp_edges += "]"
+            edges = eval(tmp_edges)
         vertices = int(request.cookies.get("vertices", None))  
         directed = bool(int(request.cookies.get("directed", None)))      
-        edges = eval(tmp_edges)
-        if not edges:
-            code = 501
+        
+        if edges is None:
+            # nie wiem co to sprawdz to dokladnie co robi 
+            flash("No edges")
+            return redirect(request.url)
         else:
             G : igraph.Graph = create_graph(n=vertices, edges=edges, directed=directed)
-            igraph.plot(G, target='myfile.pdf',
+            fig, ax = plt.subplots()
+            igraph.plot(G,
+                        target=ax,
                         vertex_label=G.vs["name"],
                         edge_label=G.es["weight"],
-                        edge_color=["lightgrey"] * len(G.es))
-            return render_template('graph_show.html', code=code, vertic = G.vs)
+                        edge_color=["lightgrey"] * len(G.es),
+                        layout="circle")
+            fig.savefig("myfile.pdf")
+            return render_template('graph_show.html', code=code, vertic = G.vs, tmp_edges=type(tmp_edges))
             
     return render_template('graph_show.html', code=code)
+
+@bp.route('/show_saved', methods=('GET', 'POST'))
+def show_saved():
+    pass
             
         
 
--- flaskr/static/script.js
+++ flaskr/static/script.js
@@ -2,7 +2,7 @@ function get_cookie(cname) {
     let name = cname + "=";
     let decodedCookie = decodeURIComponent(document.cookie);
     let ca = decodedCookie.split(';');
-    for(let i = 0; i <ca.length; i++) {
+    for(let i = 0; i < ca.length; i++) {
       let c = ca[i];
       while (c.charAt(0) == ' ') {
         c = c.substring(1);
@@ -15,25 +15,37 @@ function get_cookie(cname) {
   }
 
 function clear_cookie(){
-    document.cookie = "edges=; SameSite=None; Secure"
+    document.cookie = "edges=; SameSite=None; Secure";
 }
 
 
 function add_edge(){
-    start = document.getElementById("start").value
-    end = document.getElementById("end").value
-    weight = document.getElementById("weight").value
-    if (start !== "" && end !== "" && weight !== "") {
-        edges = get_cookie("edges");
+    let start = parseInt(document.getElementById("start").value);
+    let end = parseInt(document.getElementById("end").value);
+    let weight = parseInt(document.getElementById("weight").value);
+    let vertices = parseInt(get_cookie("vertices"))-1;
+    try{
+      if (!(isNaN(start) || isNaN(end) || isNaN(weight))) {
+        let edges = get_cookie("edges");
+        if (start < 0 || end < 0 || weight < 0 || start > vertices || end > vertices) throw Error;
+        document.getElementById("error").style.display = "none";
         if (edges !== ""){
-            document.cookie = `edges=${edges}[${start},${end},"${weight}"],; SameSite=None; Secure`
+            document.cookie = `edges=${edges}[${start},${end},"${weight}"],; SameSite=None; Secure`;
         }
         else{
-            document.cookie =`edges=[[${start},${end},"${weight}"],; SameSite=None; Secure`
+            document.cookie =`edges=[[${start},${end},"${weight}"],; SameSite=None; Secure`;
         }
         
+      }
+      else{
+        throw Error
+      }
+    }catch(Error){
+      console.error("dupa");
+      document.getElementById("error").style.display = "block";
     }
-    console.log(document.cookie)
+    
+    console.log(document.cookie);
 }
 
 
