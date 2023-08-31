77cdf9a45d52f2442f84c3533af07da53be84bdc
Siitrev <paplata50@gmail.com>
2023-08-31 12:01:19 +0200

    File removal

--- PathFinder/__init__.py
+++ PathFinder/__init__.py
@@ -1,10 +1,12 @@
-import os
+import os, atexit, datetime, time, shutil, logging
 
 from flask import Flask, render_template,request, make_response
-
+from apscheduler.schedulers.background import BackgroundScheduler
 
 def create_app(test_config=None):
     # create and configure the app
+    
+    logging.basicConfig(format="%(asctime)s  %(name)s  %(levelname)s: %(message)s")
     UPLOAD_FOLDER = "./PathFinder/files/"
     app = Flask(__name__, instance_relative_config=True)
     
@@ -19,7 +21,11 @@ def create_app(test_config=None):
     from . import graph
     app.register_blueprint(graph.bp)
     
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(func=print_date_time, trigger="interval", seconds=10)
+    scheduler.start()
     
+    atexit.register(lambda: scheduler.shutdown())
     
     @app.route('/', methods=("GET","POST"))
     def index():
@@ -29,3 +35,17 @@ def create_app(test_config=None):
         return resp
 
     return app
+
+
+def print_date_time():
+    current_time = datetime.datetime.now()
+    for i in os.listdir("./PathFinder/download"):
+        file_creation_time = os.path.getctime(f"./PathFinder/download/{i}")
+        file_creation_date = time.ctime(file_creation_time)
+        file_creation_datetime = datetime.datetime.strptime(file_creation_date,"%a %b %d %H:%M:%S %Y")
+        time_delta : datetime.timedelta = current_time-file_creation_datetime
+        if time_delta.seconds >= 0:
+            shutil.rmtree(f"./PathFinder/files/{i[:-4]}", ignore_errors=True)
+            logging.info(f"Removed folder: {i[:-4]}")
+            os.remove(f"./PathFinder/download/{i}")
+            logging.info(f"Removed zip file: {i}")
